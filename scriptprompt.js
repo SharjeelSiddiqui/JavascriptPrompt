0b850453bede015a4d1d243f60e15ade1ada910d
New User Name <creative_15@live.com>
2016-09-11 14:21:46 +0500

    1st commit and complete addition calculator by prompt

---
+++ scriptprompt.js
@@ -0,0 +1,12 @@
+// assignment No#1.
+
+var a = prompt ("Please enter your Name","Your Name");
+alert("Welcome "+a)
+
+// assignment No#2.
+// A small calculator, practise about number. "only addition"
+
+var a = parseFloat ( prompt("Please enter the 1st number for addition","First Number"));
+var b = parseFloat ( prompt("Please enter the 2nd number for addition","Second Number"));
+var total = (a+b);
+document.write ("Your answer is: "+total);
